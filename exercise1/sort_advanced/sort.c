81b76cf2bf8eb3d10003bfe613189d7a91a948e9
jxc1090 <jxc1090@student.bham.ac.uk>
2019-10-09 22:49:52 +0100

    Actually read the assignment for part 2 this time

--- exercise1/sort_advanced/sort.c
+++ exercise1/sort_advanced/sort.c
@@ -1,69 +1,91 @@
 #include "stdlib.h"
 #include "stdio.h"
 #include "string.h"
+#include "stdbool.h"
 
-char out_buf[21];
+#define RESIZE_MULTIPLIER 2
 
-int strcmp_void(const void *str1, const void *str2) {
-	return strcmp((char *) str1, (char *) str2);
-}
+int strings_size = 10;
+char **strings = NULL; //Array of string pointers, resized as necessary
+int strings_last = -1; //Last populated index
 
+int bigbuffer_size = 10;
+char *bigbuffer = NULL;
 
-int main(int argc, char ** argv) {
-	size_t strings_length = 10;
-	char *strings = (char *) malloc(strings_length * 21);
-	const realloc_multiplier = 2;
-
-	if (!strings)
-		return 1;
-
-	int i = 0;
-	while (true) {
-		if (i >= strings_length) { //Realloc input buffer
-			size_t length_new = strings_length * realloc_multiplier;
-			char *strings_new = (char *) malloc(length_new * 21);
-
-			if (!strings_new)
-				return 1;
-
-			memcpy(strings_new, strings, strings_length * 21);
-			free(strings);
-			strings = strings_new;
-			strings_length = length_new;
-		}
-
-		char *read_to = (char *) (strings + i); //addr of next string
-		if (!fgets(read_to, 21, stdin)) {
-			i--;
-			break;
-		}
-
-		for (int j = 0; j < 20; j++) {
-			if (strings[i][j] == '\n') { //Remove the \n
-				strings[i][j] = '\0';
-				break;
-			}
-		}
-
-		i++;
-	}
-
-	qsort(strings, i + 1, 21, strcmp_void);
-
-	for (int i2 = 0; i2 < i + 1; i2++) {
-		for (int j2 = 0; j2 < 20; j2++) {
-			char c = strings[i2][j2];
-			if (!c) {
-				out_buf[j2] = '\n'; //Add back the \n
-				out_buf[j2 + 1] = '\0';
-				break;
-			}
-
-			out_buf[j2] = c;
-		}
-		printf("%s", out_buf);
-	}
+void cleanup() {
+	for (int i = 0; i <= strings_last; i++)
+		free(strings[i]);
 
 	free(strings);
+	free(bigbuffer);
+}
+
+int string_comparator(const void *s1, const void *s2) { //These will be pointers to the strings array (i.e. pointers to pointers)
+	char **str1 = (char **) s1;
+	char **str2 = (char **) s2;
+	return strcmp(*str1, *str2);
+}
+
+int main(int argc, char ** argv) {
+	strings = (char **) malloc(sizeof(char *) * strings_size);
+	bigbuffer = (char *) malloc(bigbuffer_size);
+
+	int i_next = 0; //Character position in line
+	while (true) {
+		int next = getchar();
+
+		if (next != '\n' && next != EOF) {
+			if (i_next >= bigbuffer_size) { //Resize bigbuffer
+				bigbuffer_size *= RESIZE_MULTIPLIER;
+				char *bigbuffer_new = (char *) realloc(bigbuffer, bigbuffer_size);
+				if (!bigbuffer_new) {
+					cleanup();
+					return -1;
+				}
+				bigbuffer = bigbuffer_new;
+			}
+
+			char c = next;
+			bigbuffer[i_next++] = c; //Write string to bigbuffer first (so most lines will only need one malloc)
+		} else { //Transfer line out of bigbuffer
+			
+			if (strings_last + 1 >= strings_size) { //Make space for another string pointer
+				strings_size *= RESIZE_MULTIPLIER;
+				char **strings_new = (char **) realloc(strings, strings_size * sizeof(char *));
+				if (!strings_new) {
+					cleanup();
+					return -1;
+				}
+				strings = strings_new;
+			}
+
+			char *string = (char *) malloc(sizeof(char[i_next + 1])); //We need +1 to append a \0
+			if (!string) {
+				cleanup();
+				return -1;
+			}
+
+			memcpy(string, bigbuffer, i_next);
+			string[i_next] = '\0';
+
+			strings[++strings_last] = string;
+
+			if (next == EOF) {
+				if (i_next == 0) //Empty last line detection
+					free(strings[strings_last--]); //Ctrl+Z
+
+				break;
+			}
+
+			i_next = 0;
+		}
+	}
+
+	qsort(strings, strings_last + 1, sizeof(char *), string_comparator);
+
+	for (int i = 0; i <= strings_last; i++)
+		printf("%s\n", strings[i]);
+
+	cleanup();
 	return 0;
 }

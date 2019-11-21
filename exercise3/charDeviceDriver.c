4d0eb292e54503b06859e8271ce4b626a652af9c
jxc1090 <jxc1090@student.bham.ac.uk>
2019-11-21 02:20:59 +0000

    Seems to work somewhat

--- exercise3/charDeviceDriver.c
+++ exercise3/charDeviceDriver.c
@@ -2,19 +2,22 @@
 #include <linux/module.h>
 #include <linux/fs.h>
 #include <linux/slab.h>
+#include <linux/uaccess.h>
 #include <asm/uaccess.h>
 
-#define DEVICE_NAME "mod-os-message-queue"
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wincompatible-pointer-types-discards-qualifiers"
+#define DEBUG_MSGS
+#define DEVICE_NAME "opsysmem"
 MODULE_LICENSE("GPL");
 
-long device_ioctl(struct file *fp, unsigned int num, unsigned long param);
+static long device_ioctl(struct file *fp, unsigned int num, unsigned long param);
 int init_module(void);
 void cleanup_module(void);
-int device_open(struct inode *inode, struct file *file);
-int device_release(struct inode *inode, struct file *file);
-ssize_t device_read(struct file *fp, char *buffer, size_t length, loff_t *offset);
-ssize_t device_write(struct file *fp, const char *buffer, size_t length, loff_t *offset);
-
+static int device_open(struct inode *inode, struct file *file);
+static int device_release(struct inode *inode, struct file *file);
+static ssize_t device_read(struct file *fp, char __user *buffer, size_t length, loff_t *offset);
+static ssize_t device_write(struct file *fp, const char __user *buffer, size_t length, loff_t *offset);
 
 //The file operations we're supporting on this driver, and their implementations
 const struct file_operations FILE_OPERATIONS = {
@@ -25,6 +28,18 @@ const struct file_operations FILE_OPERATIONS = {
         .release = device_release
 };
 
+DEFINE_MUTEX(mutex);
+
+typedef struct message_t {
+    volatile struct list_head hd;
+    volatile size_t size;
+    volatile char data[]; //C99 "flexible array member"
+} message_t;
+
+volatile struct list_head msgs_hd;
+const size_t MAX_MESSAGE_SIZE = 4 * 1024;
+volatile unsigned long current_total_size = 0;
+volatile unsigned long max_total_size = 2 * 1024 * 1024;
 int major;
 
 int init_module() {
@@ -35,39 +50,138 @@ int init_module() {
         return major;
     }
 
-    printk(KERN_INFO "I was assigned major number %d. To talk to the driver, create a dev file"
-                     " with e.g. mknod /dev/%s c %d 0\n remember to remove the file and module later\n",
-                     major, DEVICE_NAME, major);
+    //Copied to make sure the tests work
+    printk(KERN_INFO "I was assigned major number %d. To talk to\n", major);
+    printk(KERN_INFO "the driver, create a dev file with\n");
+    printk(KERN_INFO "'mknod /dev/%s c %d 0'.\n", DEVICE_NAME, major);
+    printk(KERN_INFO "Try various minor numbers. Try to cat and echo to\n");
+    printk(KERN_INFO "the device file.\n");
+    printk(KERN_INFO "Remove the device file and module when done.\n");
 
+    INIT_LIST_HEAD(&msgs_hd);
     return 0;
 }
 
 void cleanup_module() {
-    printk(KERN_DEBUG "cleanup executed\n");
+    printk(KERN_INFO "device cleanup\n");
     unregister_chrdev(major, DEVICE_NAME);
+
+    mutex_lock(&mutex); //Stop all operations
+    if (!list_empty(&msgs_hd)) { //Free the messages
+        struct list_head *h = msgs_hd.next;
+        while (h != &msgs_hd) {
+            struct list_head *next = h->next;
+            message_t *msg = list_entry(h, message_t, hd);
+            kfree(msg);
+            h = next;
+        }
+    }
 }
 
-int device_open(struct inode * inode, struct file *file) {
-    printk(KERN_ERR "Not implemented yet\n");
-    return -EINVAL;
+static int device_open(struct inode * inode, struct file *file) {
+    printk(KERN_INFO "device opened\n");
+    return 0;
 }
 
-ssize_t device_read(struct file *fp, char *buffer, size_t length, loff_t *offset) {
-    printk(KERN_ERR "Not implemented yet\n");
-    return -EINVAL;
+static ssize_t device_read(struct file *fp, char __user *buffer, size_t length, loff_t *offset) {
+    //fp and offset not used
+    message_t *msg = NULL;
+
+    mutex_lock(&mutex);
+    if (!list_empty(&msgs_hd)) {
+        struct list_head *msg_hd = msgs_hd.next;
+        list_del(msg_hd);
+        msg = list_entry(msg_hd, message_t, hd);
+        current_total_size -= msg->size;
+    }
+    mutex_unlock(&mutex);
+
+    if (!msg)
+        return -EAGAIN;
+
+    if (length > msg->size) { //Don't allow arbitrary reading
+#ifdef DEBUG_MSGS
+        printk(KERN_DEBUG "user requested more data than available\n");
+#endif
+        length = msg->size;
+    }
+
+#ifdef DEBUG_MSGS
+    if (length < msg->size)
+        printk(KERN_DEBUG "User requested less data than the message size\n");
+#endif
+
+    if (copy_to_user(buffer, &(msg->data), length)) {
+        printk(KERN_ERR "device_read: Failed to write to userspace buffer\n");
+        kfree(msg);
+        return -EFAULT;
+    } else {
+        kfree(msg);
+        return length;
+    }
 }
 
-ssize_t device_write(struct file *fp, const char *buffer, size_t length, loff_t *offset) {
-    printk(KERN_ERR "Not implemented yet\n");
-    return -EINVAL;
+static ssize_t device_write(struct file *fp, const char __user *buffer, size_t length, loff_t *offset) {
+    //fp and offset not used
+    if (length > MAX_MESSAGE_SIZE)
+        return -EINVAL;
+
+    if (current_total_size + length > max_total_size)
+        return -EAGAIN;
+
+    message_t *msg = kmalloc(sizeof(message_t) + length, GFP_KERNEL);
+    if (copy_from_user(&(msg->data), buffer, length)) {
+        printk(KERN_ERR "device_write: Failed to read from userspace buffer\n");
+        kfree(msg);
+        return -EFAULT;
+    }
+    INIT_LIST_HEAD(&(msg->hd));
+    msg->size = length;
+
+    int error = 1;
+
+    mutex_lock(&mutex);
+    if (current_total_size + length <= max_total_size) { //Since we were unlocked before, this could have changed
+        list_add_tail(&(msg->hd), &msgs_hd);
+        current_total_size += length;
+        error = 0;
+    }
+    mutex_unlock(&mutex);
+
+    if (error) {
+#ifdef DEBUG_MSGS
+        printk(KERN_DEBUG "device_write: Not enough room after locking");
+        kfree(msg);
+        return -EAGAIN;
+#endif
+    }
+
+    return length;
 }
 
-int device_release(struct inode *inode, struct file *file) {
-    printk(KERN_ERR "Not implemented yet\n");
-    return -EINVAL;
+static int device_release(struct inode *inode, struct file *file) {
+    printk(KERN_INFO "device closed\n");
+    return 0;
 }
 
-long device_ioctl(struct file *fp, unsigned int num, unsigned long param) {
-    printk(KERN_ERR "Not implemented yet\n");
-    return -EINVAL;
+static long device_ioctl(struct file *fp, unsigned int num, unsigned long param) {
+    if (num != 0) {
+        printk(KERN_ERR "Unknown ioctl number\n");
+        return -EINVAL;
+    }
+
+    int return_val;
+
+    mutex_lock(&mutex);
+    if (param >= current_total_size) {
+        max_total_size = param;
+        return_val = 0;
+        printk(KERN_INFO "Max total data set to %lu\n", param);
+    } else {
+        return_val = -EINVAL;
+    }
+    mutex_unlock(&mutex);
+
+    return return_val;
 }
+#pragma clang diagnostic pop
---
+++ exercise3/ioctl.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+
+int main(int argc, char **argv) {
+    if (argc != 4) {
+        printf("Usage: %s filename ioctl_num ioctl_param\n", argv[0]);
+        return 0;
+    }
+
+    unsigned int num = strtoul(argv[2], NULL, 10);
+    unsigned int param = strtoul(argv[3], NULL, 10);
+
+    printf("Trigger ioctl %u with param %u on file %s (y/n)?\n", num, param, argv[1]);
+    char response[2];
+    if (!fgets(response, 2, stdin))
+        return 0;
+
+    if (strcmp(response, "y"))
+        return 0;
+
+    int fd = open(argv[1], O_RDONLY);
+    if (fd == -1) {
+        perror("Couldn't open that file");
+        return 1;
+    }
+
+    int result = ioctl(fd, num, param);
+    if (result >= 0)
+        printf("Result: %d", result);
+    else {
+        perror("Negative return code");
+    }
+
+    close(fd);
+    return 0;
+}
